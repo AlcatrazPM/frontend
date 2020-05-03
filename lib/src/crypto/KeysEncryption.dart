d8cb0c127fc9524b3a2ceb6057cc0739cc49c2d3
Vlad Nastase <vladdnastase@gmail.com>
2020-05-03 14:04:34 +0300

    Added basic encryption

--- lib/src/crypto/KeysEncryption.dart
+++ lib/src/crypto/KeysEncryption.dart
@@ -1,5 +1,12 @@
-abstract class KeysEncryption{
-  Future<String> dataEncryptionKey();
-  Future<String> keyEncryptionKey(String password);
+import '../accounts/model/Account.dart';
+
+abstract class KeysEncryption {
+  Future<String> generateDataEncryptionKey();
+  Future<String> encrypt(String value, String key); // Returns initial value concatenated to the encrypted value
+  Future<String> decrypt(String value, String key);
+  Future<String> generateKEKSalt(); // This is stored in the DB, so it must be sent to the backend
+  Future<String> generateKeyEncryptionKey(String password, String salt);
   Future<String> passwordHash(String password, int iterations);
+  Future<Account> decryptEntry(Account entry, String DEK);
+  Future<List<Account>> decryptAll(List<Account> entries, String DEK);
 }
